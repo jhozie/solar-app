bb529765d39b4360c1251d995d197424eeafd085
Joseph Nwankwo <josephcgs02@gmail.com>
2025-03-14 08:28:30 -0500

    eslint

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Only run ESLint on these directories during production builds
+    dirs: ['src/app', 'src/components']
+  }
+}
+
+module.exports = nextConfig 
